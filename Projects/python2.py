b4b5aa02b51a8b4c26a0f2d98fb494ba2076a303
Moises1200 <moisesmejia9@gmail.com>
2019-11-19 21:34:52 -0600

    dd

--- Projects/python2.py
+++ Projects/python2.py
@@ -1,4 +1,14 @@
-
+#This will return the number of charcters in the string
 parrot = "Norwegian Blue"
 len(parrot)
 print(len(parrot))
+
+#################################################################################################################################
+#This will lower case evertthing in Parrot
+
+parrot = "Norwegian Blue"
+
+print (parrot.lower())
+
+#################################################################################################################################
+
